1b710b30c2d3962529e587858e376c1837ef7d9b
nishu91020 <rainishu111@gmail.com>
2021-06-17 23:31:23 +0530

    Auth UI fix-1

--- src/Screens/Signup/index.js
+++ src/Screens/Signup/index.js
@@ -3,7 +3,6 @@ import Header from '../../Components/Header';
 import { Form } from '../../Components/Form';
 import { Link } from 'react-router-dom';
 import './styles.css';
-import { AuthProvider, useAuth } from '../../Context/AuthContext';
 const Signup = () => {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
@@ -12,33 +11,26 @@ const Signup = () => {
     return (
         <div className="container">
             <Header />
-            <AuthProvider>
-                <Form.AuthModal className="card">
-                    <Form.Title heading="Signup" />
-                    <Form.InputField
-                        label="Username"
-                        value={username}
-                        onChange={e => setUserName(e.target.value)}
-                        type="text"
-                    />
-                    <Form.InputField
-                        label="Email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        type="email"
-                    />
-                    <Form.InputField
-                        label="Password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        type="password"
-                    />
-                    <p>
-                        Already have an account?<Link to="/Login">Login</Link>
-                    </p>
-                    <Form.BtnForm content="Signup" />
-                </Form.AuthModal>
-            </AuthProvider>
+            <Form.AuthModal className="card">
+                <Form.Title heading="Signup" />
+                <Form.InputField
+                    label="Username"
+                    value={username}
+                    onChange={e => setUserName(e.target.value)}
+                    type="text"
+                />
+                <Form.InputField label="Email" value={email} onChange={e => setEmail(e.target.value)} type="email" />
+                <Form.InputField
+                    label="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                />
+                <p>
+                    Already have an account?<Link to="/Login">Login</Link>
+                </p>
+                <Form.BtnForm content="Signup" />
+            </Form.AuthModal>
         </div>
     );
 };
