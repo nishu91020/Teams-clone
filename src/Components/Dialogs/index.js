b369010aae0c868c92d8c4c510b5edc91fb070be
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2021-07-08 02:50:02 +0530

    dialog box done correctly

--- src/Components/Dialogs/index.js
+++ src/Components/Dialogs/index.js
@@ -1,7 +1,87 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { MoreVert } from '@material-ui/icons';
+import { Menu, MenuItem, Dialog, DialogContent, DialogTitle, DialogActions, TextField, Button } from '@material-ui/core';
 const Dialogs = () => {
-    return <div />;
+    const [ isJoinDialogOpen, setIsJoinDialogOpen ] = useState(false);
+    const [ isCreateDialogOpen, setIsCreateDialogOpen ] = useState(false);
+    const [ isJoinMessageDialogOpen, setIsJoinMessageDialogOpen ] = useState(false);
+    const [ anchorEl, setAnchorEl ] = useState(null);
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const handleJoinDialogClose = () => {
+        setIsJoinDialogOpen(false);
+    };
+    const handleJoinDialog = () => {
+        setIsJoinDialogOpen(true);
+    };
+    const handleCreateDialogClose = () => {
+        setIsCreateDialogOpen(false);
+    };
+    const handleCreateDialog = () => {
+        setIsCreateDialogOpen(true);
+    };
+    const handleJoinMessageDialogClose = () => {
+        setIsJoinMessageDialogOpen(false);
+    };
+    const handleJoinMessageDialog = () => {
+        setIsJoinMessageDialogOpen(true);
+    };
+    return (
+        <div>
+            <MoreVert onClick={handleClick} />
+            <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                <MenuItem onClick={handleJoinDialog}>Create Meeting</MenuItem>
+                <MenuItem onClick={handleCreateDialog}>Join Meeting</MenuItem>
+                <MenuItem onClick={handleJoinMessageDialog}>Join Chats</MenuItem>
+            </Menu>
+            <Dialog open={isCreateDialogOpen} onClose={handleCreateDialogClose}>
+                <DialogTitle>Crete a room</DialogTitle>
+                <DialogContent>
+                    <TextField label=" Meeting Title" />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCreateDialogClose} color="primary">
+                        Close
+                    </Button>
+                    <Button onClick={handleCreateDialogClose} color="primary">
+                        Create
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={isJoinDialogOpen} onClose={handleJoinDialogClose}>
+                <DialogTitle>Join Room</DialogTitle>
+                <DialogContent>
+                    <TextField variant="outlined" label="Room ID" />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleJoinDialogClose} color="primary">
+                        Close
+                    </Button>
+                    <Button onClick={handleJoinDialogClose} color="primary">
+                        Join
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={isJoinMessageDialogOpen} onClose={handleJoinMessageDialogClose}>
+                <DialogTitle>Join Room</DialogTitle>
+                <DialogContent>
+                    <TextField variant="outlined" label="Room ID" />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleJoinMessageDialogClose} color="primary">
+                        Close
+                    </Button>
+                    <Button onClick={handleJoinMessageDialogClose} color="primary">
+                        Join ChatRoom
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 };
 
 export default Dialogs;
--- src/Components/MeetingChatList/index.js
+++ src/Components/MeetingChatList/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-import { Grid, makeStyles, Menu, MenuItem } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import MeetingChatCard from '../MeetingChatCard';
-import { MoreVert } from '@material-ui/icons';
-import JoinDialog from '../JoinDialog';
-import CreateDialog from '../CreateDialog';
-import JoinMessage from '../JoinMessage';
+import Dialogs from '../Dialogs';
 import './styles.css';
 
 const useStyles = makeStyles({
@@ -24,47 +21,12 @@ const useStyles = makeStyles({
 });
 const MeetingChatList = () => {
     const classes = useStyles();
-    const [ isJoinDialogOpen, setIsJoinDialogOpen ] = useState(false);
-    const [ isCreateDialogOpen, setIsCreateDialogOpen ] = useState(false);
-    const [ isJoinMessageDialogOpen, setIsJoinMessageDialogOpen ] = useState(false);
-    const [ anchorEl, setAnchorEl ] = useState(null);
-    const handleClick = event => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const handleJoinDialogClose = () => {
-        setIsJoinDialogOpen(false);
-    };
-    const handleJoinDialog = () => {
-        setIsJoinDialogOpen(true);
-        <JoinDialog handleClose={handleJoinDialogClose} open={isJoinDialogOpen} />;
-    };
-    const handleCreateDialogClose = () => {
-        setIsCreateDialogOpen(false);
-    };
-    const handleCreateDialog = () => {
-        setIsCreateDialogOpen(true);
-        <CreateDialog handleClose={handleCreateDialogClose} open={isCreateDialogOpen} />;
-    };
-    const handleJoinMessageDialogClose = () => {
-        setIsJoinMessageDialogOpen(false);
-    };
-    const handleJoinMessageDialog = () => {
-        setIsJoinMessageDialogOpen(true);
-        <JoinMessage handleClose={handleJoinMessageDialogClose} open={isJoinMessageDialogOpen} />;
-    };
+
     return (
         <Grid container item style={{ height: '100%', overflowY: 'hidden' }}>
             <Grid container item className={classes.chatContainer} alignItems="center" justify="space-between">
                 Chats
-                <MoreVert onClick={handleClick} />
-                <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-                    <MenuItem onClick={handleJoinDialog}>Create Meeting</MenuItem>
-                    <MenuItem onClick={handleCreateDialog}>Join Meeting</MenuItem>
-                    <MenuItem onClick={handleJoinMessageDialog}>Join Chats</MenuItem>
-                </Menu>
+                <Dialogs />
             </Grid>
 
             <Grid container item className={classes.meetingList}>
